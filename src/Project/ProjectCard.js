308e7cc31fdc54ac27e085ded708dd2ec163e80f
Christina Park <christinapark0117@gmail.com>
2020-06-22 17:10:14 -0400

    burger diner game card added

--- src/Project/ProjectCard.js
+++ src/Project/ProjectCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BreadIsGold from "../Media/Card Img for Bread Is Gold .png"
+import BurgerDiner from "../Media/Card Img for Burger Diner.png"
 
 const ProjectCard = () => {
     return (
@@ -10,9 +11,20 @@ const ProjectCard = () => {
                 <div className="card-body">
                     <h5 className="card-title">Bread Is Gold</h5>
                     <p className="card-text">
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                        Bread Is Gold is an app based on a recipe book written by an award winning Chef Massimo Bottura and his 60 other chef friends.
                     </p>
+                    <button className='button'>Details</button>
+                </div>
+            </div>
+
+            <div className="card">
+                <img className="card-img burgerdiner" alt="Project Image" src={BurgerDiner}/>
+                <div className="card-body">
+                    <h5 className="card-title">Burger Diner</h5>
+                    <p className="card-text">
+                        Burger Diner is a game where you have to make a burger with the given recipe.
+                    </p>
+                    <button className='button'>Details</button>
                 </div>
             </div>
 
