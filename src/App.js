40b09e232e541407b51dbee4003887c64472b68c
Christina Park <christinapark0117@gmail.com>
2020-06-22 16:07:44 -0400

    media all updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar'
 import HomeContainer from "./Home/HomeContainer";
