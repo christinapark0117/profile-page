6ec1d51dae23f8730e0c09947874bc575c0f22b6
Christina Park <christinapark0117@gmail.com>
2020-06-25 00:59:40 -0400

    some changes due to styling

--- src/Project/BreadIsGold.js
+++ src/Project/BreadIsGold.js
@@ -1,8 +1,29 @@
 import React from "react";
+// import {Carousel} from "bootstrap"
+import '../PageStyle/BreadIsGold.css';
+import BIGHome from '../Media/Bread is Gold Home.jpg';
+import BIGWelcome from '../Media/Bread Is Gold Welcome Page.jpg'
+
 
 const BreadIsGold = () => {
     return (
         <div id="eachProjectPage">
+
+            <h3 className='pageTitle'>Bread Is Gold</h3>
+
+            {/*<div className='carousel'>*/}
+            {/*    <div className='carousel-item active'>*/}
+            {/*         <img className="w-100" alt="Project Image" src={BIGHome}/>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+
+            {/*<div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">*/}
+            {/*    <div className="carousel-inner">*/}
+            {/*        <div className="carousel-item active">*/}
+            {/*            <img src={BIGHome} className="d-block w-100" alt="..."/>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
 
         </div>
     )
--- src/Project/BurgerDiner.js
+++ src/Project/BurgerDiner.js
@@ -1,8 +1,17 @@
 import React from "react";
+import '../PageStyle/BurgerDiner.css'
+
 
 const BurgerDiner = () => {
     return (
-        <div id="eachProjectPage">
+        <div id="projectDetailPage">
+            <h3 className='pageHeader'>Burger Diner</h3>
+
+            <div className='carousel'>
+                <div className='carousel-item'>
+                    {/*<img className="d-block w-100" alt='Burger Diner Home' src={}/>*/}
+                </div>
+            </div>
 
         </div>
     )
