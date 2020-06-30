cca61da5f6f59315e0698af36f0625f681d722c2
Christina Park <christinapark0117@gmail.com>
2020-06-30 15:55:52 -0400

    Bread Is Gold detail page styling done

--- src/Project/BreadIsGold.js
+++ src/Project/BreadIsGold.js
@@ -6,7 +6,7 @@ import BIGMyRecipes from '../Media/Bread Is Gold My Recipes.jpg'
 import BIGRecipeDetail from '../Media/Bread Is Gold Recipe Detail Page.jpg'
 import BIGRecipesList from '../Media/Bread Is Gold Recipes List.jpg'
 import BIGChefList from '../Media/Bread Is Gold Chefs Page.png'
-import Carousel from 'react-bootstrap/Carousel';
+import {Carousel, ListGroup} from 'react-bootstrap';
 
 
 const BreadIsGold = () => {
@@ -18,36 +18,68 @@ const BreadIsGold = () => {
             <h3 className='pageTitle'>Bread Is Gold</h3>
 
 
-            <Carousel indicators={false}>
+                <Carousel indicators={false}>
 
 
-                <Carousel.Item>
-                    <img src={BIGHome} className="d-block w-100" alt="Bread Is Gold home page"/>
-                </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={BIGHome} className="d-block w-100" alt="Bread Is Gold home page"/>
+                    </Carousel.Item>
 
-                <Carousel.Item>
-                    <img src={BIGWelcome} className="d-block w-100" alt="Bread Is Gold welcome"/>
-                </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={BIGWelcome} className="d-block w-100" alt="Bread Is Gold welcome"/>
+                    </Carousel.Item>
 
-                <Carousel.Item>
-                    <img src={BIGMyRecipes} className="d-block w-100" alt="Bread Is Gold welcome"/>
-                </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={BIGMyRecipes} className="d-block w-100" alt="Bread Is Gold welcome"/>
+                    </Carousel.Item>
 
-                <Carousel.Item>
-                    <img src={BIGRecipeDetail} className="d-block w-100" alt="Bread Is Gold welcome"/>
-                </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={BIGRecipeDetail} className="d-block w-100" alt="Bread Is Gold welcome"/>
+                    </Carousel.Item>
 
-                <Carousel.Item>
-                    <img src={BIGRecipesList} className="d-block w-100" alt="Bread Is Gold welcome"/>
-                </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={BIGRecipesList} className="d-block w-100" alt="Bread Is Gold welcome"/>
+                    </Carousel.Item>
 
-                <Carousel.Item>
-                    <img src={BIGChefList} className="d-block w-100" alt="Bread Is Gold welcome"/>
-                </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={BIGChefList} className="d-block w-100" alt="Bread Is Gold welcome"/>
+                    </Carousel.Item>
 
 
-            </Carousel>
+                </Carousel>
 
+
+
+            <div className='project-description'>
+
+                    <div className='frontend'>
+
+                        <h5 className='pageTitle2'>Frontend</h5>
+
+                        <ListGroup variant="flush" id='front'>
+                            <ListGroup.Item>JavaScript & React</ListGroup.Item>
+                            <ListGroup.Item>Custom CSS</ListGroup.Item>
+                            <ListGroup.Item>Imported data by fetching to the backend</ListGroup.Item>
+                            <ListGroup.Item>Includes search function</ListGroup.Item>
+                        </ListGroup>
+
+                    </div>
+
+
+                    <div className='backend'>
+
+                        <h5 className='pageTitle3'>Backend</h5>
+
+                        <ListGroup variant="flush" id='back'>
+                            <ListGroup.Item>Ruby on Rails</ListGroup.Item>
+                            <ListGroup.Item>PostgreSQL</ListGroup.Item>
+                            <ListGroup.Item>Serialized RESTful API</ListGroup.Item>
+                            <ListGroup.Item>MVC Pattern</ListGroup.Item>
+                        </ListGroup>
+
+                    </div>
+
+            </div>
 
         </div>
     )
--- src/Project/BurgerDiner.js
+++ src/Project/BurgerDiner.js
@@ -1,16 +1,57 @@
 import React from "react";
 import '../PageStyle/BurgerDiner.css'
+import {Carousel, ListGroup} from "react-bootstrap";
+import BDDirections from "../Media/Card Img for Burger Diner.png"
 
 
 const BurgerDiner = () => {
-    return (
-        <div id="projectDetailPage">
-            <h3 className='pageHeader'>Burger Diner</h3>
 
-            <div className='carousel'>
-                <div className='carousel-item'>
-                    {/*<img className="d-block w-100" alt='Burger Diner Home' src={}/>*/}
+    return(
+        <div class="eachProjectPage">
+
+
+            <h3 className='pageTitle'>Burger Diner</h3>
+
+
+            <Carousel indicators={false}>
+
+
+                <Carousel.Item>
+                    <img src={BDDirections} className="d-block w-100" alt="Bread Is Gold home page"/>
+                </Carousel.Item>
+
+
+            </Carousel>
+
+
+
+            <div className='project-description'>
+
+                <div className='frontend'>
+
+                    <h5 className='pageTitle2'>Frontend</h5>
+
+                    <ListGroup variant="flush" id='front'>
+                        <ListGroup.Item>JavaScript (DOM Manipulation)</ListGroup.Item>
+                        <ListGroup.Item>Semantic UI + Custom CSS + keyframes</ListGroup.Item>
+                    </ListGroup>
+
                 </div>
+
+
+                <div className='backend'>
+
+                    <h5 className='pageTitle3'>Backend</h5>
+
+                    <ListGroup variant="flush" id='back'>
+                        <ListGroup.Item>Ruby on Rails</ListGroup.Item>
+                        <ListGroup.Item>SQLite3</ListGroup.Item>
+                        <ListGroup.Item>RESTful API</ListGroup.Item>
+                        <ListGroup.Item>MVC Pattern</ListGroup.Item>
+                    </ListGroup>
+
+                </div>
+
             </div>
 
         </div>
