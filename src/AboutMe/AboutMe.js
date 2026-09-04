4b9f2dd63380b037529fa7b23df1e0ff2d31ff31
christinapark0117 <christinapark0117@gmail.com>
2026-09-04 16:00:35 -0400

    updated about me and about me css

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -24,7 +24,9 @@ const AboutMe = () => {
                             Who is Christina?
                         </div>
                         <div>
-                            A junior software engineer with expertise in Ruby on Rails and Javascript, React and Redux
+                            <p>Christina is a full-stack software engineer with experience building and supporting web applications across both the public and private sectors. Her background spans front-end and back-end development, with experience working in Agile teams and collaborating across engineering, product, and business functions.</p>
+                            
+                            <p>She is expecially interested in creating thoughtful, user-centered products and bringing together technology, design, and problem-solving.</p>
                         </div>
                     </div>
 
@@ -33,7 +35,10 @@ const AboutMe = () => {
                             What's Her Background?
                         </div>
                         <div>
-                            <p>Christina majored in Oboe Performance in college. She was a former Symphony Orchestra member, a teacher, and a school administrator. Always interested in art and design, she started designing websites as a hobby. She thought that it would be better if she could bring the websites that she designed alive on her own, so she decided to switch her career to software engineering. She started her career as a backend developer at <a href='https://www.beachbody.com/en/product/fitness_programs/homepage-v2.do?code=SEMB_BOD_GOOGLE&utm_medium=SEM&utm_source=GOOGLE&utm_campaign=BB_US_SEM_GGL_Brand_Core_Exact&utm_content=14092422213_124793629905&utm_term=beachbody_&gclid=CjwKCAjwyaWZBhBGEiwACslQowPhy0ejOjqZzrsw98xXHk18owF9qTqLxSW0tOO36TaMWLJpNljuOBoCLawQAvD_BwE&gclsrc=aw.ds'>Beachbody</a> and is currently working at <a href='https://www.fdic.gov/'>FDIC (Federal Deposit Insurance Corporation)</a> redesigning and rebuilding their .gov and intranet site. It’s been a great experience so far, and she’d like to find opportunities that can expand her knowledge of her current expertise as a full stack web developer.</p>
+                            <p>Christina's path into technology began in an entirely different field: music. She earned her degree in Oboe Performance and worked professionally as a musician before transitioning into education, where she spent several years teaching and working in school administration.</p>
+                            <p>Her long-standing interest in art, design, and technology eventually led her to software engineering. What began with an interest in designing websites grew into a desire to understand how to build them from the ground up.</p>
+                            <p>Sje began her software career in backend deveolopment at Beachbody and later expanded into full-stack engineering, working on projects for organizations including federal government clients such as <a href='https://www.fdic.gov/'>FDIC</a>, <a href='https://www.fhfa.gov/'>FHFA</a> and the <a href='https://www.irs.gov/'>IRS</a>. Along the way she has worked with cross-functional Agile teams and gained experience across application development, APIs, databases, front-end interfaces, and modern web technologies.</p>
+                            <p>Today, Christina brings together the creativity and discipline of her musical background, the communication skills she developed as an educator, and her technical experience as a software engineer. She is particularly interested in opportunities where she can contribute not only to how products are build but also to how they are shaped and improved.</p>
                         </div>
                     </div>
 
@@ -42,12 +47,14 @@ const AboutMe = () => {
                             Technical Skills
                         </div>
                         <div className="bioSkills">
-                            <i className="bioSkill"/> Ruby on Rails<br/>
-                            <i className="bioSkill"/> Javascript, React, Redux<br/>
-                            <i className="bioSkill"/> HTML5, CSS, Semantic UI, Bootstrap <br/>
+                            <i className="bioSkill"/> Javascript, React<br/>
+                            <i className="bioSkill"/> PHP, Drupal <br/>
+                            <i className="bioSkill"/> HTML5, CSS, SCSS, Semantic UI, Bootstrap <br/>
                             <i className="bioSkill"/> SQL, SQLite3, PostgreSQL<br/>
-                            <i className="bioSkill"/> RESTful architecture, MVC pattern<br/>
-                            <i className="bioSkill"/> Git, APIs
+                            <i className="bioSkill"/> REST APIs, API Integration<br/>
+                            <i className="bioSkill"/> Git, Github, Bitbucket<br/>
+                            <i className="bioSkill"/> Agile, Scrum<br/>
+
                         </div>
                     </div>
                 </div>
