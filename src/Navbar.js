5446b596385953d520fd05bd092d9f3d791d12f6
christinapark0117 <christinapark0117@gmail.com>
2026-09-07 20:40:21 -0400

    minor link change for the navbar aboutme

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
 
                 <div className="navLinks">
                     <NavLink exact className="navLink" activeClassName="navLinkActive" to="/">Home</NavLink>
-                    <NavLink className="navLink" activeClassName="navLinkActive" to="/about">About</NavLink>
+                    <NavLink className="navLink" activeClassName="navLinkActive" to="/aboutme">About</NavLink>
                     <NavLink className="navLink" activeClassName="navLinkActive" to="/experience">Experience</NavLink>
                     <NavLink className="navLink" activeClassName="navLinkActive" to="/projects">Projects</NavLink>
                     <NavLink className="navLink" activeClassName="navLinkActive" to="/skills">Skills</NavLink>
