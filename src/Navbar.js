57cd1cbf185d6d560bf50775835457223bf3cfa4
christinapark0117 <christinapark0117@gmail.com>
2026-09-13 13:25:16 -0400

    navbar code updated for responsiveness

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import '../src/PageStyle/Navbar.css'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const closeMenu = () => setIsOpen(false)
+
     return (
         <nav id="navbar">
             <div className="navInner">
-                <Link className="navBrand" to="/">CHRISTINA M. PARK</Link>
+                <Link className="navBrand" to="/" onClick={closeMenu}>CHRISTINA M. PARK</Link>
 
-                <div className="navLinks">
-                    <NavLink exact className="navLink" activeClassName="navLinkActive" to="/">Home</NavLink>
-                    <NavLink className="navLink" activeClassName="navLinkActive" to="/aboutme">About</NavLink>
-                    <NavLink className="navLink" activeClassName="navLinkActive" to="/experience">Experience</NavLink>
-                    <NavLink className="navLink" activeClassName="navLinkActive" to="/project">Projects</NavLink>
-                    <NavLink className="navLink" activeClassName="navLinkActive" to="/skills">Skills</NavLink>
-                    <NavLink className="navLink" activeClassName="navLinkActive" to="/contact">Contact</NavLink>
+                <button
+                    className={`navToggle ${isOpen ? 'navToggleOpen' : ''}`}
+                    onClick={() => setIsOpen(!isOpen)}
+                    aria-label="Toggle navigation menu"
+                    aria-expanded={isOpen}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+
+                <div className={`navLinks ${isOpen ? 'navLinksOpen' : ''}`}>
+                    <NavLink exact className="navLink" activeClassName="navLinkActive" to="/" onClick={closeMenu}>Home</NavLink>
+                    <NavLink className="navLink" activeClassName="navLinkActive" to="/aboutme" onClick={closeMenu}>About</NavLink>
+                    <NavLink className="navLink" activeClassName="navLinkActive" to="/experience" onClick={closeMenu}>Experience</NavLink>
+                    <NavLink className="navLink" activeClassName="navLinkActive" to="/project" onClick={closeMenu}>Projects</NavLink>
+                    <NavLink className="navLink" activeClassName="navLinkActive" to="/skills" onClick={closeMenu}>Skills</NavLink>
+                    <NavLink className="navLink" activeClassName="navLinkActive" to="/contact" onClick={closeMenu}>Contact</NavLink>
                 </div>
             </div>
         </nav>
